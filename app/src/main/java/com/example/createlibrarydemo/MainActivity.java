8c79ddbf9ba607000f384d5151e816a366c13b44
Arvind Kumar <ak010cse@gmail.com>
2020-04-07 10:51:40 +0530

    Toast

---
+++ app/src/main/java/com/example/createlibrarydemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.createlibrarydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "onCreate:", Toast.LENGTH_SHORT).show();
+    }
+}
